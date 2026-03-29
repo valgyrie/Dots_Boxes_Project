217d3bbdf42eafaa4d482081f9c4eca045888392
Mohammad Barakeh <barakim25@gmail.com>
2026-03-29 20:23:00 +0300

    Modify checkSquare function to include pointer parameter

--- playgame.h
+++ playgame.h
@@ -2,5 +2,5 @@ typedef struct {
 	int up, down, left, right;
 } Point;
 void addLine(Point arr[5][6], int row, int row2, int col, int col2);
-int checkSquare(Point arr[5][6], int row, int row2, int col, int col2);
+int checkSquare(Point arr[5][6], int row, int row2, int col, int col2, int* p);
 void printGrid(Point arr[5][6], char a[100]);
