36c53a4b8b888109e4f018dd56fa7067996fe1ca
Mohammad Barakeh <barakim25@gmail.com>
2026-03-29 20:20:54 +0300

    Refactor main function and improve scoring logic

--- main.c
+++ main.c
@@ -15,8 +15,12 @@ int main() {
     int turn_count = 1;
     int score_A = 0, score_B = 0, squares_count = 0, t = 0;
     char scores[100];
-    memset(scores, ' ', 100);
+    int i;
+for (i = 0; i < 100; i++) {
+    scores[i] = ' ';
+}
     char currentPlayer = (turn_count % 2 == 0) ? 'B' : 'A';
+    int x =0;
     while (squares_count < 20) {
         
         printf("\nPlayer %c, enter row1 col1 row2 col2: ", currentPlayer);
@@ -25,12 +29,12 @@ int main() {
         
 
         addLine(grid, row1, row2, col1, col2);
-        int result = checkSquare(grid, row1, row2, col1, col2);
+        int result = checkSquare(grid, row1, row2, col1, col2,&x);
 
         if (result > 0) {
             scores[t++] = currentPlayer;
             squares_count++;
-            if (currentPlayer == 'A') score_A++; else score_B++;
+            if (currentPlayer == 'A') score_A+=x; else score_B+=x;
             printf("Good turn player %c\n\n",currentPlayer);
             
         }
