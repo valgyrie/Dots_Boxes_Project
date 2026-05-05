a4379a597b8097aae81ab8fc453de8a55295a0d0
mah <maa393@mail.aub.edu>
2026-05-05 10:39:45 +0300

    Delete playgame.h

--- playgame.h
+++
@@ -1,6 +0,0 @@
-typedef struct {
-	int up, down, left, right;
-} Point;
-int addLine(Point arr[5][6], int row, int row2, int col, int col2);
-int checkSquare(Point arr[5][6], int row, int row2, int col, int col2, int* p);
-void printGrid(Point arr[5][6], char a[100]);
